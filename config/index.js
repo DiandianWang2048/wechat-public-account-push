1925ff77665de81850cebdd22bd97fbb969d123c
DiandianWang2048 <88967096+DiandianWang2048@users.noreply.github.com>
2022-08-29 19:21:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx3471db3194e574c2",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "612afa5c0fe69eff30719e8e6389d7f4",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "M92VG_7T9ANX77dWDX7CMKTvcjOcXHNaXYuxXLE9xBQ",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["okCsI6MT8IZbg68cmY1pipjiK2hY"],
      
     /**
      * 信息配置
@@ -19,24 +19,22 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "山东",
     // 所在城市
-    CITY: "惠州",
+    CITY: "临沂",
 
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "小婷", "year": "2001", "date": "03-04"},
+      {"name": "点点", "year": "2001", "date": "01-10"},
     ],
 
     /** 日期相关 */
 
-    // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    // 认识的日子，格式同上
+    LOVE_DATE: "2021-12-16",
 
 
     /** 好文节选 */
@@ -53,9 +51,9 @@ export const config = {
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
-// {{birthday_message.DATA}}
+// 今天是我们认识的第{{love_day.DATA}}天
+// {{birthday1.DATA}}
+// {{birthday2.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
